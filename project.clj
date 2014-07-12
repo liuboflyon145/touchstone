325f1bf684efe81e3b30e5e910ff2ff75c53ae15
Peter Taoussanis <ptaoussanis@gmail.com>
2014-07-12 16:46:14 +0700

    Bump deps

--- project.clj
+++ project.clj
@@ -12,8 +12,8 @@
 
   :dependencies
   [[org.clojure/clojure            "1.4.0"]
-   [com.taoensso/encore            "0.9.7"]
-   [com.taoensso/carmine           "2.4.6"]
+   [com.taoensso/encore            "1.7.0"]
+   [com.taoensso/carmine           "2.6.2"]
    [org.clojure/math.combinatorics "0.0.7"]]
 
   :profiles
@@ -21,16 +21,16 @@
    :server-jvm {:jvm-opts ^:replace ["-server"]}
    :1.5  {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6  {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}
-   :test {:dependencies [[expectations            "1.4.56"]
-                         [org.clojure/test.check  "0.5.7"]
-                         [ring/ring-core          "1.2.1"]]
+   :test {:dependencies [[expectations            "2.0.7"]
+                         [org.clojure/test.check  "0.5.8"]
+                         [ring/ring-core          "1.3.0"]]
           :plugins [[lein-expectations "0.0.8"]
                     [lein-autoexpect   "1.2.2"]]}
    :dev
    [:1.6 :test
     {:plugins [[lein-pprint  "1.1.1"]
-               [lein-ancient "0.5.4"]
-               [codox        "0.6.7"]]}]}
+               [lein-ancient "0.5.5"]
+               [codox        "0.8.10"]]}]}
 
   :test-paths ["test" "src"]
   :aliases
